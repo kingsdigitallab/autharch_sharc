bf8240b5c6ebcd277b24c53744a7d71f75db4df0
Jamie Norrish <jamie@artefact.org.nz>
2020-10-15 20:26:29 +1300

    Initial commit of cookiecutter Django product.

---
+++ autharch_sharc/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from autharch_sharc.users.models import User
+from autharch_sharc.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
