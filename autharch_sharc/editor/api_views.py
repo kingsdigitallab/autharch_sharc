bb43438d1f60ad7ddd14fe044d2e07ef9a5b3050
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-11-18 11:38:54 +0000

    added api serializer and view initial commit

--- autharch_sharc/editor/api_views.py
+++ autharch_sharc/editor/api_views.py
@@ -1,4 +1,28 @@
-from rest_framework import filters, viewsets
+from rest_framework import permissions
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    IdsFilterBackend,
+    OrderingFilterBackend,
+    DefaultOrderingFilterBackend,
+    SearchFilterBackend,
+)
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from .documents import EADDocument
+from .serializers import EADDocumentSerializer
 
 
+
+class EADDocumentViewSet(DocumentViewSet):
+    document = EADDocument
+    serializer_class = EADDocumentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    lookup_field = "pk"
+
+    filter_backends = [
+        FilteringFilterBackend,
+        IdsFilterBackend,
+        OrderingFilterBackend,
+        DefaultOrderingFilterBackend,
+        SearchFilterBackend,
+    ]
--- autharch_sharc/editor/serializers.py
+++ autharch_sharc/editor/serializers.py
@@ -1,3 +1,24 @@
-from rest_framework import serializers
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+
+from .documents import EADDocument
 
 
+class EADDocumentSerializer(DocumentSerializer):
+    """ Serializer for EAD XML document"""
+
+    class Meta:
+        document = EADDocument
+
+        fields = (
+            'creators'
+            'pk',
+            'archdesc_level',
+            'category',
+            'connection_primary',
+            'connection_secondary',
+            'connection_type',
+            'date_of_acquisition',
+            'date_of_creation',
+            'publicationstatus_value',
+            'unittitle'
+        )
