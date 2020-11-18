6a4a4a5d2d08c3dff7645bab4cb1f98a615dba1c
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-11-18 16:04:54 +0000

    adding facets, not complete

--- autharch_sharc/editor/api_views.py
+++ autharch_sharc/editor/api_views.py
@@ -5,8 +5,15 @@ from django_elasticsearch_dsl_drf.filter_backends import (
     OrderingFilterBackend,
     DefaultOrderingFilterBackend,
     SearchFilterBackend,
+    FacetedSearchFilterBackend
 )
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from elasticsearch_dsl import (
+    DateHistogramFacet,
+    RangeFacet,
+    TermsFacet,
+)
+
 from .documents import EADDocument
 from .serializers import EADDocumentSerializer
 
@@ -24,6 +31,7 @@ class EADDocumentViewSet(DocumentViewSet):
         OrderingFilterBackend,
         DefaultOrderingFilterBackend,
         SearchFilterBackend,
+        FacetedSearchFilterBackend
     ]
 
     search_fields = (
@@ -38,14 +46,42 @@ class EADDocumentViewSet(DocumentViewSet):
     filter_fields = {
         'pk': 'pk',
         'unittitle': 'unittitle.raw',
-        'date_of_creation': 'date_of_creation'
+        'date_of_creation': 'date_of_creation',
+        'category': 'category.raw'
+    }
+
+    faceted_search_fields = {
+        'category': {
+            'facet': TermsFacet,
+            'field': 'category.raw',
+            'enabled': True
+        },
+        'connection_primary': {
+            'facet': TermsFacet,
+            'field': 'connection_primary',
+            'enabled': True
+        },
+        'date_of_creation': {
+            'field': 'date_of_creation',
+            "enabled": True,
+            'facet': DateHistogramFacet,
+            'options': {
+                'interval': 'year',
+            }
+        },
+        'date_of_acquisition': {
+            'field': 'date_of_acquisition',
+            "enabled": True,
+            'facet': DateHistogramFacet,
+            'options': {
+                'interval': 'year',
+            }
+        },
     }
 
     ordering_fields = {
         'unittitle': 'unittitle.sort',
         'date_of_creation': 'date_of_creation'
     }
-
-    
 
     ordering = ('unittitle', 'date_of_creation')
