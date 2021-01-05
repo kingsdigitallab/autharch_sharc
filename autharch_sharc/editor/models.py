95311e5a05bbe9e2c53e94d59a173bcc10ca4561
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-01-05 18:30:48 +0000

    Added stream fields for resource

--- autharch_sharc/editor/models.py
+++ autharch_sharc/editor/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django_kdl_timeline.models import AbstractTimelineEventSnippet
 from editor.documents import EADDocument
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.api import APIField
-from wagtail.core.fields import RichTextField
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 
@@ -56,6 +60,59 @@ class SharcTimelineEventSnippet(AbstractTimelineEventSnippet):
 
 register_snippet(SharcTimelineEventSnippet)
 
+"""
+Block types for resource page
+"""
+
+
+class ResourceBlock(blocks.StructBlock):
+    """ Abstract base block for other types"""
+
+    heading = blocks.CharBlock(form_classname="full title")
+    body = blocks.RichTextBlock()
+
+    class Meta:
+        abstract = True
+
+
+class ResourceDocumentBlock(ResourceBlock):
+    """ Block with a document e.g. pdf attached """
+
+    document = DocumentChooserBlock(icon="doc-full-inverse")
+
+
+class ResourceImageBlock(ResourceBlock):
+    """ Resource with image attached"""
+
+    image = ImageChooserBlock()
+
+
+class ResourceEmbedBlock(ResourceBlock):
+    """ Resource with embed attached"""
+
+    embed = EmbedBlock(icon="media")
+
+
+class StreamFieldPage(Page):
+    body = StreamField(
+        [
+            ("image", ResourceImageBlock()),
+            ("document", ResourceDocumentBlock(icon="doc-full-inverse")),
+            ("embed", ResourceEmbedBlock(icon="media")),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("body"),
+    ]
+
+    api_fields = [
+        APIField("title"),
+        APIField("body"),
+    ]
+
+    promote_panels = Page.promote_panels
+
 
 class RichTextPage(Page):
     body = RichTextField(blank=True, null=True)
@@ -64,7 +121,7 @@ class RichTextPage(Page):
         index.SearchField("body"),
     ]
 
-    subpage_types = ["RichTextPage"]
+    subpage_types = ["RichTextPage", "StreamFieldPage"]
 
     content_panels = [
         FieldPanel("title", classname="full title"),
