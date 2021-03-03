2b839d1c03edbbd5abcc151b03f4434e2ec80441
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-03-03 18:50:41 +0000

    compress enabled by default

--- config/settings/production.py
+++ config/settings/production.py
@@ -105,7 +105,7 @@ ANYMAIL = {}
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_ENABLED
-COMPRESS_ENABLED = env.bool("COMPRESS_ENABLED", default=True)
+# COMPRESS_ENABLED = env.bool("COMPRESS_ENABLED", default=True)
 COMPRESS_OFFLINE = True
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_STORAGE
 COMPRESS_STORAGE = "compressor.storage.GzipCompressorFileStorage"
