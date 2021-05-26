c843298fb83fd09d8db45eeaf78d5ba9c989f3b7
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-05-26 13:08:40 +0100

    extra router not needed

--- autharch_sharc/django_kdl_timeline/api.py
+++ autharch_sharc/django_kdl_timeline/api.py
@@ -4,10 +4,11 @@ API for timeline server through Wagtail
 https://docs.wagtail.io/en/v2.11.3/advanced_topics/api/v2/configuration.html)
 """
 
+from rest_framework import permissions
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
+from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
-from rest_framework import permissions
 
 
 class SharcPagesAPIViewSet(PagesAPIViewSet):
@@ -17,3 +18,4 @@ class SharcPagesAPIViewSet(PagesAPIViewSet):
 wagtail_api_router = WagtailAPIRouter("wagtailapi")
 wagtail_api_router.register_endpoint("images", ImagesAPIViewSet)
 wagtail_api_router.register_endpoint("pages", SharcPagesAPIViewSet)
+wagtail_api_router.register_endpoint("documents", DocumentsAPIViewSet)
