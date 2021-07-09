682d12a7020c539c30ed503979664b80cedb7961
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-07-09 10:38:57 +0100

    order in iiif

--- autharch_sharc/editor/models/iiif.py
+++ autharch_sharc/editor/models/iiif.py
@@ -10,3 +10,4 @@ class SharcIIIF(models.Model):
     rcin = models.CharField(blank=True, null=True, max_length=256)
     images_available = models.TextField(blank=True, null=True)
     department = models.CharField(blank=True, null=True, max_length=256)
+    order = models.IntegerField(default=1)
