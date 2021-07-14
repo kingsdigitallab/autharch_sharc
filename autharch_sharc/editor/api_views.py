215ee86583aaa00c313b3eaaebdd984fa7226403
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-07-14 11:29:10 +0100

    person viewset before remove

--- autharch_sharc/editor/api_views.py
+++ autharch_sharc/editor/api_views.py
@@ -302,16 +302,3 @@ class EditorTableView(APIView):
     def get(self, request, *args, **kwargs):
         results = self.document_search(request, **kwargs)
         return Response({"count": kwargs["results_count"], "results": results})
-
-
-class PersonEADDocumentViewSet(EADDocumentViewSet):
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
